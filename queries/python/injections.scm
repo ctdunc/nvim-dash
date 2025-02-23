d76be3f79875c42cb2a76540e0fbb121326ae974
Connor Duncan <connor.duncan@invesco.com>
2025-02-23 11:00:42 -0600

    Allow % formatted callbacks

--- queries/python/injections.scm
+++ queries/python/injections.scm
@@ -11,6 +11,20 @@
 	)
 )
 
+
+(call
+  (identifier) @name (#eq? @name clientside_callback) 
+  (argument_list 
+    (
+     (binary_operator
+       left:(string (string_content) 
+	     @injection.content 
+	     (#set! injection.include-children)
+	     (#set! injection.language "javascript"))
+       )
+	))
+)
+
 ; functions declared in dictionaries
 (call
   function: (identifier) @dict_id (#eq? @dict_id "dict")
